7a453b4c58634dfadcf16fd92b3c6aeb5f4bb4a4
AnnikaWespe <annika.wespe@gmail.com>
2016-11-29 11:11:29 +0100

    Add files via upload

--- LoopTiming.js
+++ LoopTiming.js
@@ -1,3 +1,10 @@
+var statusColorMap = {
+	rot: "red",
+	gelb: "yellow",
+	gruen: "green",
+	blau: "blue",
+	lila: "pink"
+}
 $(document).ready(function() {
 	var date = new Date();
 	var thisYear = date.getFullYear();
@@ -21,7 +28,7 @@ var getTablesReady = function() {
 	var tableCells = "<tr class = 'tableCells'>";
 
 	for (var i = 0; i < 52; i++) {
-		kalenderWochen += "<th>" + i + "</td>";
+		kalenderWochen += "<th>" + (i + 1) + "</th>";
 		tableCells += "<td id = 'row0column" + i + "'></td>";
 	};
 	kalenderWochen += "</th>";
@@ -46,7 +53,7 @@ var setTableData = function(year0, year1, year2, year3) {
 		$SP().list("Dummy Liste Dev Loop").get(function(data) {
 			numberOfRows = data.length;
 			for (var i = 0; i < numberOfRows; i++) {
-				var currentStartKW = new Number(data[i].getAttribute("gkqu"));
+				var currentStartKW = new Number(data[i].getAttribute("gkqu")) - 1;
 				var currentEndKW = new Number(data[i].getAttribute("vc3x"));
 				var currentEndYear = new Number(data[i].getAttribute("ubmk"));
 				var currentStartYear = new Number(data[i].getAttribute("gj8q"));
@@ -54,17 +61,37 @@ var setTableData = function(year0, year1, year2, year3) {
 				var currentStatus = data[i].getAttribute("Statusfarbe");
 				var yearMin = new Number(year0);
 				var yearMax = new Number(year3);
-				console.log(" " + currentEndKW + " " + currentStartYear + " " + currentStatus + " " + currentTitle + " " + currentStartKW + " " + currentEndYear);
-				if (currentStartYear < yearMin) {
-					if (currentEndYear == year0) {
-						for (var j = 0; j < currentEndKW; j++) {
-							$(".year" + year0).find("[id='row0column" + j + "']").css("background-color", "green");
-						}
-						alert(currentStatus);
+				if (currentStartYear < yearMin && currentEndYear == year0) {
+					markBeginningOfYear(year0, currentEndKW, currentStatus);
+				} else if (currentEndYear > yearMax && currentStartYear == year3) {
+					markEndOfYear(year3, currentStartKW, currentStatus);
+				} else if (yearMin <= currentStartYear && currentEndYear <= yearMax) {
+					if (currentStartYear < currentEndYear) {
+						markEndOfYear(currentStartYear, currentStartKW, currentStatus);
+						markBeginningOfYear(currentEndYear, currentEndKW, currentStatus);
+					} else {
+						markInYear(currentStartYear, currentStartKW, currentEndKW, currentStatus);
 					}
-
-				}
+				};
 			};
 		});
 	})
+}
+
+var markBeginningOfYear = function(year, KW, status) {
+	for (var j = 0; j < KW; j++) {
+		$(".year" + year).find("[id='row0column" + j + "']").css("background-color", statusColorMap[status]);
+	};
+}
+
+var markEndOfYear = function(year, KW, status) {
+	for (var j = KW; j < 52; j++) {
+		$(".year" + year).find("[id='row0column" + j + "']").css("background-color", statusColorMap[status]);
+	}
+}
+var markInYear = function(year, start, end, status) {
+	for (var j = start; j < end; j++) {
+		$(".year" + year).find("[id='row0column" + j + "']").css("background-color", statusColorMap[status]);
+	};
+	console.log(year + " " + start + " " + end);
 }
